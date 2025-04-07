9419614d2e8d5845e5c0e245713d74ffa754c191
Chris Hennes <chennes@gmail.com>
2025-04-07 16:27:33 -0500

    Adjust init scripts for being an external mod

--- InitGui.py
+++ InitGui.py
@@ -1,13 +1,9 @@
 # SPDX-License-Identifier: LGPL-2.1-or-later
-# AddonManager gui init module
-# (c) 2001 Juergen Riegel
-# License LGPL
 
+# InitGui.py is run automatically by FreeCAD during the GUI initialization process
+
+import os
 import AddonManager
 
-FreeCADGui.addLanguagePath(":/translations")
+FreeCADGui.addLanguagePath(f"{os.path.dirname(__file__)}/Resources/translations")
 FreeCADGui.addCommand("Std_AddonMgr", AddonManager.CommandAddonManager())
-
-import FreeCAD
-
-FreeCAD.__unit_test__ += ["TestAddonManagerGui"]
