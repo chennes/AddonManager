f91a803fa202ebd184230c838420d8aeeca52389
Chris Hennes <chennes@gmail.com>
2025-04-08 14:16:44 -0500

    Update tests to return exit code

--- AddonManagerTest/run_all_tests.py
+++ AddonManagerTest/run_all_tests.py
@@ -1,7 +1,11 @@
+# SPDX-License-Identifier: LGPL-2.1-or-later
+
+import sys
 import unittest
 
 if __name__ == "__main__":
     loader = unittest.TestLoader()
     suite = loader.discover(start_dir=".", pattern="test_*.py")
     runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(suite)
+    result = runner.run(suite)
+    sys.exit(not result.wasSuccessful())
